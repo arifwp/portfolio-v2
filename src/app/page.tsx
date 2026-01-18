b1745863e19620b26b4137f771d6c2b4dd082de0
Arif Wahyu <arifwahyu696@gmail.com>
2026-01-18 21:09:57 +0700

    split components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,142 +1,18 @@
-"use client";
-
-import { ContainerLayout } from "@/components/ContainerLayout";
-import { motion } from "framer-motion";
+import { ContainerLayout } from "@/components/containers/ContainerLayout";
+import { SectionAbout } from "@/sections/SectionAbout";
+import { SectionHome } from "@/sections/SectionHome";
+import { SectionProjectsV2 } from "@/sections/SectionProjectsV2";
 
 export default function Home() {
   return (
     <ContainerLayout>
-      <section id="home" className="w-full h-dvh p-6">
-        <div className="w-full h-full flex flex-col items-center justify-between">
-          <div />
+      <SectionHome />
 
-          <div className="gap-2 md:gap-4 xl:gap-8 flex flex-col text-center">
-            <div className="overflow-hidden">
-              <motion.h1
-                initial={{ y: "100%" }}
-                animate={{ y: 0 }}
-                transition={{
-                  duration: 0.7,
-                  ease: [0.22, 1, 0.36, 1], // easeOut cubic-bezier (premium feel)
-                }}
-                className="text-md sm:text-xl md:text-2xl 2xl:text-4xl font-light"
-              >
-                {"Hi! I'm Arif Prasetyo"}
-              </motion.h1>
-            </div>
+      <SectionAbout />
 
-            <motion.h2
-              initial="hidden"
-              animate="show"
-              variants={{
-                hidden: {},
-                show: {
-                  transition: { staggerChildren: 0.15 },
-                },
-              }}
-              className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl font-bold leading-none"
-            >
-              <div className="overflow-hidden">
-                <motion.span
-                  variants={{
-                    hidden: { y: "100%" },
-                    show: { y: 0 },
-                  }}
-                  transition={{
-                    duration: 0.7,
-                    ease: [0.22, 1, 0.36, 1],
-                  }}
-                  className="block"
-                >
-                  VISUAL POETRY TRAPPED
-                </motion.span>
-              </div>
+      {/* <SectionProjects /> */}
 
-              <div className="pb-2 overflow-hidden">
-                <motion.span
-                  variants={{
-                    hidden: { y: "120%" },
-                    show: { y: 0 },
-                  }}
-                  transition={{
-                    duration: 0.7,
-                    ease: [0.22, 1, 0.36, 1],
-                  }}
-                  className="block"
-                >
-                  {"INSIDE AN ENGINEER'S MIND"}
-                </motion.span>
-              </div>
-            </motion.h2>
-          </div>
-
-          <motion.div
-            initial="hidden"
-            animate="show"
-            variants={{
-              hidden: {},
-              show: {
-                // transition: { staggerChildren: 0.15 },
-              },
-            }}
-            className="w-full gap-0 text-lg xl:text-2xl font-semibold flex flex-col lg:flex-row items-center justify-between"
-          >
-            <div className="overflow-hidden">
-              <motion.h4
-                variants={{
-                  hidden: { y: "100%" },
-                  show: {
-                    y: 0,
-                  },
-                }}
-                transition={{
-                  duration: 0.7,
-                  ease: [0.22, 1, 0.36, 1],
-                }}
-              >
-                FULL STACK ENGINEER
-              </motion.h4>
-            </div>
-
-            <motion.div className="gap-2 flex flex-row items-center">
-              <div className="overflow-hidden">
-                <motion.h4
-                  variants={{
-                    hidden: { y: "100%" },
-                    show: {
-                      y: 0,
-                    },
-                  }}
-                  transition={{
-                    duration: 0.7,
-                    ease: [0.22, 1, 0.36, 1],
-                  }}
-                  className="text-neutral-500 font-medium"
-                >
-                  BASED IN
-                </motion.h4>
-              </div>
-
-              <div className="overflow-hidden">
-                <motion.h4
-                  variants={{
-                    hidden: { y: "100%" },
-                    show: {
-                      y: 0,
-                    },
-                  }}
-                  transition={{
-                    duration: 0.7,
-                    ease: [0.22, 1, 0.36, 1],
-                  }}
-                >
-                  BANDUNG, INDONESIA
-                </motion.h4>
-              </div>
-            </motion.div>
-          </motion.div>
-        </div>
-      </section>
+      <SectionProjectsV2 />
     </ContainerLayout>
   );
 }
